0d899c173c535d7de5def65f4f251d5fd8a47ea9
imcoffeefreak <subramanyarao4@gmail.com>
2020-10-27 09:39:52 +0530

    Added CS-3C

---
+++ 21-10-2020/Basics of JS/CS-3C/varibales.js
@@ -0,0 +1,14 @@
+function add(num1, num2) {
+    let result = num1 + num2;
+    console.log("result is = " + result);
+}
+
+add = (num1, num2) => {
+    let result = num1 + num2;
+    console.log("result is = " + result);
+}
+
+add(5, 6)
+add(10, 20)
+
+arrow("This is arrow function")
